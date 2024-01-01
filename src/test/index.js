239bd59060c1e815ad31b0638139b427c46fd3b6
DR-wyl <wylpeter@qq.com>
2024-01-01 20:39:08 +0800

    update

--- src/test/index.js
+++ src/test/index.js
@@ -1,282 +1,246 @@
-// interface MixinType {
-//     [x: string]: number;
-//     [y: number]: number;
-// }
-// interface MixinType1 {
-//     [x: string]: Object;
-//     [y: number]: number;
-// }
-// interface ReadOnlyArr {
-//     readonly [index: number]: number
-// }
-// let arr000: ReadOnlyArr = [1, 2, 3];
-// let arr011: ReadonlyArray<number> = [1, 2, 3];
-// // arr000[0]=100;
-// // arr011[0]=100;
-// /* 
-//     同时使用两种类型的索引
-//     number索引签名声明的值，需是string索引签名声明的值的子类型
-// */
-// // interface AnimalClass{
-// //     name:string;
-// //     eat(food:string):void;
-// // }
-// // class Animal implements AnimalClass{
-// //     name = 'dog';
-// //     eat(food:string){
-// //         return;
-// //     }
-// // }
-// // 继承多个接口
-// interface Ani {
-//     name: string;
-// }
-// interface Sw {
-//     height: number;
-// }
-// interface Dog extends Ani, Sw {
-//     color: string;
-// }
-// let d1: Dog = { color: 'red', name: 'dog', height: 100 };
-// // 混合类型
-// // interface MixinObj{
-// //     (x:number,y:number):void;
-// //     uname:string;
-// //     say():void;
-// // }
-// // let person000:MixinObj =<MixinObj>function(x:number,y:number) {
-// //     return ;
-// // }
-// // person000.uname = 'person';
-// // person000.say = function() {}
-// interface Counter {
-//     (start: number): string;
-//     interval: number;
-//     reset(): void;
-// }
-// function gerCounter(): Counter {
-//     let counter = <Counter>function (start: number) { };
-//     counter.interval = 123;
-//     counter.reset = function () { };
-//     return counter;
-// }
-// class Person {
-//     public uname = 'jack';
-//     private money = 100;
-//     protected age = 100;
-//     public say() {
-//         console.log(this.uname);
-//         console.log(this.money);
-//     }
-// }
-// class Son extends Person {
-//     log() {
-//         // console.log(this.money); // 编译报错
-//         console.log(this.age);
-//     }
-// }
-// const p001 = new Person();
-// const s001 = new Son();
-// console.log(p001.uname);
-// // console.log(s001.money); // 编译报错
-// s001.say();
-// // console.log(s001.money); // 编译报错
-// class Person1 {
-//     protected constructor() { }
-// }
-// // const p002 = new Person1(); // 编译报错
-// class Son2 extends Person1 {
-//     constructor() {
-//         super();
-//     }
-// }
-// const s002 = new Son2();
-// // 只读
-// class Person2 {
-//     readonly uname: string;
-//     constructor(uname: string) {
-//         this.uname = uname;
-//     }
-// }
-// const p003 = new Person2('jack');
-// console.log(p003.uname);
-// class Person3 {
-//     constructor(public uname: string, private readonly age: number) { }
-// }
-// const p0002 = new Person3('jack', 18);
-// // 存取器
-// class Person4 {
-//     private gender: number = 1;
-//     // getGender(){
-//     //     return this.gender;
-//     // }
-//     // setGender(gender:number){
-//     //     this.gender = gender;
-//     // }
-//     // 优化后写法
-//     get Gender() {
-//         return this.gender;
-//     }
-//     set Gender(gender: number) {
-//         this.gender = gender;
-//     }
-// }
-// const p0004 = new Person4();
-// console.log(p0004.Gender);
-// p0004.Gender = 0;
-// console.log(p0004.Gender);
-// // 静态属性
-// class Person5 {
-//     static uname: string = 'jack';
-//     static say() {
-//         console.log('hello');
-//     }
-// }
-// console.log(Person5.uname);
-// interface IPerson6 {
-//     uname: string;
-//     say(a: number): void;
-// }
-// // 抽象类
-// abstract class Person6 {
-//     uname: string;
-//     abstract say(a: number): void;
-//     say1() {
-//         console.log('hello');
-//     }
-// }
-// class Son6 extends Person6 {
-//     say(a: number): void {
-//         console.log('hello say');
-//     }
-// }
-// class SonI6 implements IPerson6 {
-//     uname: string;
-//     say(a: number): void {
-//         console.log('hello say');
-//     }
-// }
-// class Person7 {
-//     uname: string;
-//     age: number;
-//     constructor(uname: string, age: number) {
-//         this.uname = uname;
-//         this.age = age;
-//     }
-// }
-// interface IPerson7 extends Person7 {
-//     say(): void;
-//     log(): void;
-// }
-// let obj0001: IPerson7 = {
-//     uname: 'jack',
-//     age: 18,
-//     say() {
-//         console.log('hello say');
-//     },
-//     log() {
-//         console.log('hello log');
-//     }
-// }
-// let addFun: (a: number, b: number) => number;
-// addFun = function (a: number, b: number) {
-//     return a;
-// }
-// addFun = function (a: number) {
-//     return a;
-// }
-// // addFun(1) // 编译报错
-// addFun(1, 2)
-// interface IAdd {
-//     (a: number, b: number): void;
-// }
-// let addFun01 = function (a: number, b: number): number {
-//     return a + b
-// }
-// // 默认值
-// interface IAdd2 {
-//     (a: number, b?: number): void;
-// }
-// let addFun02 = function (a: number, b: number = 100): number {
-//     return a + b
-// }
-// addFun02(1, 2)
-// addFun02(1)
-// // 剩余参数
-// interface IAdd3 {
-//     (a: number, b?: number, ...rest: number[]): void;
-// }
-// let addFun03 = function (a: number, b: number = 100, ...rest: number[]): number {
-//     return a
-// }
-// addFun03(1, 2, 3, 4, 5)
-// addFun03(1, 2)
-// addFun03(1)
-// // 函数重载
-// function log1(x: number): number;
-// function log1(x: string): string;
-// function log1(x: any): any {
-//     if (typeof x === 'number') {
-//         return 10 * x;
-//     } else if (typeof x === 'string') {
-//         return 'hello' + x;
-//     }
-// };
-// console.log(log1(2));
-// console.log(log1('jack'));
-// // 泛型
-// function log<T>(x: T): T {
-//     // console.log(x);
-//     return x;
-// }
-// log<string>('hello');
-// log<number>(123);
-// function swap<T, U>(arr: [T, U]) {
-//     return [arr[1], arr[0]]
-// }
-// console.log(swap<number, number>([1, 2]));
-// console.log(swap<string, string>(['a', 'b']));
-// console.log(swap<string, number>([' a ', 1]));
-// function getLen<T extends { length: number }>(x: T) {
-//     return x.length;
-// }
-// getLen({ length: 10 });
-// getLen([]);
-// interface User1<T, U> {
-//     uname: string;
-//     age: T;
-//     gender: U;
-// }
-// let u1: User1<number, string> = {
-//     uname: 'jack',
-//     age: 20,
-//     gender: 'male'
-// }
-// interface User2<T, U> {
-//     uname: string;
-//     age: number;
-//     gender: U;
-// }
-// interface User {
-//     uname: string;
-//     age: number;
-// }
-// interface Func1<T> {
-//     <K extends User>(x: T, y: K): T;
-// }
-// let say: Func1<number>;
-// say = function <K extends User>(x: number, y: K): number {
-//     return y.age;
-// }
-// // 类中使用泛型
-// class Users<T = string> {
-//     name: T;
-//     constructor(name: T) {
-//         this.name = name;
-//     }
-// }
-// const p002 = new Users<string>('jack')
-// console.log(p002.name);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var arr000 = [1, 2, 3];
+var arr011 = [1, 2, 3];
+var d1 = { color: 'red', name: 'dog', height: 100 };
+function gerCounter() {
+    var counter = function (start) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+var Person = /** @class */ (function () {
+    function Person() {
+        this.uname = 'jack';
+        this.money = 100;
+        this.age = 100;
+    }
+    Person.prototype.say = function () {
+        console.log(this.uname);
+        console.log(this.money);
+    };
+    return Person;
+}());
+var Son = /** @class */ (function (_super) {
+    __extends(Son, _super);
+    function Son() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Son.prototype.log = function () {
+        // console.log(this.money); // 编译报错
+        console.log(this.age);
+    };
+    return Son;
+}(Person));
+var p001 = new Person();
+var s001 = new Son();
+console.log(p001.uname);
+// console.log(s001.money); // 编译报错
+s001.say();
+// console.log(s001.money); // 编译报错
+var Person1 = /** @class */ (function () {
+    function Person1() {
+    }
+    return Person1;
+}());
+// const p002 = new Person1(); // 编译报错
+var Son2 = /** @class */ (function (_super) {
+    __extends(Son2, _super);
+    function Son2() {
+        return _super.call(this) || this;
+    }
+    return Son2;
+}(Person1));
+var s002 = new Son2();
+// 只读
+var Person2 = /** @class */ (function () {
+    function Person2(uname) {
+        this.uname = uname;
+    }
+    return Person2;
+}());
+var p003 = new Person2('jack');
+console.log(p003.uname);
+var Person3 = /** @class */ (function () {
+    function Person3(uname, age) {
+        this.uname = uname;
+        this.age = age;
+    }
+    return Person3;
+}());
+var p0002 = new Person3('jack', 18);
+// 存取器
+var Person4 = /** @class */ (function () {
+    function Person4() {
+        this.gender = 1;
+    }
+    Object.defineProperty(Person4.prototype, "Gender", {
+        // getGender(){
+        //     return this.gender;
+        // }
+        // setGender(gender:number){
+        //     this.gender = gender;
+        // }
+        // 优化后写法
+        get: function () {
+            return this.gender;
+        },
+        set: function (gender) {
+            this.gender = gender;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    return Person4;
+}());
+var p0004 = new Person4();
+console.log(p0004.Gender);
+p0004.Gender = 0;
+console.log(p0004.Gender);
+// 静态属性
+var Person5 = /** @class */ (function () {
+    function Person5() {
+    }
+    Person5.say = function () {
+        console.log('hello');
+    };
+    Person5.uname = 'jack';
+    return Person5;
+}());
+console.log(Person5.uname);
+// 抽象类
+var Person6 = /** @class */ (function () {
+    function Person6() {
+    }
+    Person6.prototype.say1 = function () {
+        console.log('hello');
+    };
+    return Person6;
+}());
+var Son6 = /** @class */ (function (_super) {
+    __extends(Son6, _super);
+    function Son6() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    Son6.prototype.say = function (a) {
+        console.log('hello say');
+    };
+    return Son6;
+}(Person6));
+var SonI6 = /** @class */ (function () {
+    function SonI6() {
+    }
+    SonI6.prototype.say = function (a) {
+        console.log('hello say');
+    };
+    return SonI6;
+}());
+var Person7 = /** @class */ (function () {
+    function Person7(uname, age) {
+        this.uname = uname;
+        this.age = age;
+    }
+    return Person7;
+}());
+var obj0001 = {
+    uname: 'jack',
+    age: 18,
+    say: function () {
+        console.log('hello say');
+    },
+    log: function () {
+        console.log('hello log');
+    }
+};
+var addFun;
+addFun = function (a, b) {
+    return a;
+};
+addFun = function (a) {
+    return a;
+};
+// addFun(1) // 编译报错
+addFun(1, 2);
+var addFun01 = function (a, b) {
+    return a + b;
+};
+var addFun02 = function (a, b) {
+    if (b === void 0) { b = 100; }
+    return a + b;
+};
+addFun02(1, 2);
+addFun02(1);
+var addFun03 = function (a, b) {
+    if (b === void 0) { b = 100; }
+    var rest = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        rest[_i - 2] = arguments[_i];
+    }
+    return a;
+};
+addFun03(1, 2, 3, 4, 5);
+addFun03(1, 2);
+addFun03(1);
+function log1(x) {
+    if (typeof x === 'number') {
+        return 10 * x;
+    }
+    else if (typeof x === 'string') {
+        return 'hello' + x;
+    }
+}
+;
+console.log(log1(2));
+console.log(log1('jack'));
+// 泛型
+function log(x) {
+    // console.log(x);
+    return x;
+}
+log('hello');
+log(123);
+function swap(arr) {
+    return [arr[1], arr[0]];
+}
+console.log(swap([1, 2]));
+console.log(swap(['a', 'b']));
+console.log(swap([' a ', 1]));
+function getLen(x) {
+    return x.length;
+}
+getLen({ length: 10 });
+getLen([]);
+var u1 = {
+    uname: 'jack',
+    age: 20,
+    gender: 'male'
+};
+var say;
+say = function (x, y) {
+    return y.age;
+};
+// 类中使用泛型
+var Users = /** @class */ (function () {
+    function Users(name) {
+        this.name = name;
+    }
+    return Users;
+}());
+var p002 = new Users('jack');
+console.log(p002.name);
 // 枚举
 var Direction;
 (function (Direction) {
--- src/test/index.ts
+++ src/test/index.ts
@@ -1,349 +1,349 @@
-// interface MixinType {
-//     [x: string]: number;
-//     [y: number]: number;
-// }
-// interface MixinType1 {
-//     [x: string]: Object;
-//     [y: number]: number;
-// }
+interface MixinType {
+    [x: string]: number;
+    [y: number]: number;
+}
+interface MixinType1 {
+    [x: string]: Object;
+    [y: number]: number;
+}
 
-// interface ReadOnlyArr {
-//     readonly [index: number]: number
-// }
+interface ReadOnlyArr {
+    readonly [index: number]: number
+}
 
-// let arr000: ReadOnlyArr = [1, 2, 3];
-// let arr011: ReadonlyArray<number> = [1, 2, 3];
+let arr000: ReadOnlyArr = [1, 2, 3];
+let arr011: ReadonlyArray<number> = [1, 2, 3];
 
-// // arr000[0]=100;
-// // arr011[0]=100;
-
-
-// /* 
-//     同时使用两种类型的索引
-//     number索引签名声明的值，需是string索引签名声明的值的子类型
-// */
-
-// // interface AnimalClass{
-// //     name:string;
-// //     eat(food:string):void;
-// // }
-
-// // class Animal implements AnimalClass{
-// //     name = 'dog';
-// //     eat(food:string){
-// //         return;
-// //     }
-// // }
+// arr000[0]=100;
+// arr011[0]=100;
 
 
-// // 继承多个接口
-// interface Ani {
-//     name: string;
+/* 
+    同时使用两种类型的索引
+    number索引签名声明的值，需是string索引签名声明的值的子类型
+*/
+
+// interface AnimalClass{
+//     name:string;
+//     eat(food:string):void;
 // }
 
-// interface Sw {
-//     height: number;
-// }
-
-// interface Dog extends Ani, Sw {
-//     color: string;
-// }
-
-// let d1: Dog = { color: 'red', name: 'dog', height: 100 };
-
-
-// // 混合类型
-
-// // interface MixinObj{
-// //     (x:number,y:number):void;
-// //     uname:string;
-// //     say():void;
-// // }
-
-// // let person000:MixinObj =<MixinObj>function(x:number,y:number) {
-// //     return ;
-// // }
-// // person000.uname = 'person';
-// // person000.say = function() {}
-
-
-// interface Counter {
-//     (start: number): string;
-//     interval: number;
-//     reset(): void;
-// }
-// function gerCounter(): Counter {
-//     let counter = <Counter>function (start: number) { };
-//     counter.interval = 123;
-//     counter.reset = function () { };
-//     return counter;
-// }
-
-// class Person {
-//     public uname = 'jack';
-//     private money = 100;
-//     protected age = 100;
-//     public say() {
-//         console.log(this.uname);
-//         console.log(this.money);
+// class Animal implements AnimalClass{
+//     name = 'dog';
+//     eat(food:string){
+//         return;
 //     }
 // }
 
-// class Son extends Person {
-//     log() {
-//         // console.log(this.money); // 编译报错
-//         console.log(this.age);
-//     }
+
+// 继承多个接口
+interface Ani {
+    name: string;
+}
+
+interface Sw {
+    height: number;
+}
+
+interface Dog extends Ani, Sw {
+    color: string;
+}
+
+let d1: Dog = { color: 'red', name: 'dog', height: 100 };
+
+
+// 混合类型
+
+// interface MixinObj{
+//     (x:number,y:number):void;
+//     uname:string;
+//     say():void;
 // }
 
-// const p001 = new Person();
-// const s001 = new Son();
-
-// console.log(p001.uname);
-// // console.log(s001.money); // 编译报错
-// s001.say();
-// // console.log(s001.money); // 编译报错
-
-// class Person1 {
-//     protected constructor() { }
+// let person000:MixinObj =<MixinObj>function(x:number,y:number) {
+//     return ;
 // }
-
-// // const p002 = new Person1(); // 编译报错
-
-// class Son2 extends Person1 {
-//     constructor() {
-//         super();
-//     }
-// }
-
-// const s002 = new Son2();
+// person000.uname = 'person';
+// person000.say = function() {}
 
 
-// // 只读
-// class Person2 {
-//     readonly uname: string;
-//     constructor(uname: string) {
-//         this.uname = uname;
-//     }
-// }
-// const p003 = new Person2('jack');
+interface Counter {
+    (start: number): string;
+    interval: number;
+    reset(): void;
+}
+function gerCounter(): Counter {
+    let counter = <Counter>function (start: number) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
 
-// console.log(p003.uname);
+class Person {
+    public uname = 'jack';
+    private money = 100;
+    protected age = 100;
+    public say() {
+        console.log(this.uname);
+        console.log(this.money);
+    }
+}
 
-// class Person3 {
-//     constructor(public uname: string, private readonly age: number) { }
-// }
+class Son extends Person {
+    log() {
+        // console.log(this.money); // 编译报错
+        console.log(this.age);
+    }
+}
 
-// const p0002 = new Person3('jack', 18);
+const p001 = new Person();
+const s001 = new Son();
 
-// // 存取器
+console.log(p001.uname);
+// console.log(s001.money); // 编译报错
+s001.say();
+// console.log(s001.money); // 编译报错
 
-// class Person4 {
-//     private gender: number = 1;
-//     // getGender(){
-//     //     return this.gender;
-//     // }
-//     // setGender(gender:number){
-//     //     this.gender = gender;
-//     // }
-//     // 优化后写法
-//     get Gender() {
-//         return this.gender;
-//     }
-//     set Gender(gender: number) {
-//         this.gender = gender;
-//     }
-// }
+class Person1 {
+    protected constructor() { }
+}
 
-// const p0004 = new Person4();
-// console.log(p0004.Gender);
-// p0004.Gender = 0;
-// console.log(p0004.Gender);
+// const p002 = new Person1(); // 编译报错
 
-// // 静态属性
-// class Person5 {
-//     static uname: string = 'jack';
-//     static say() {
-//         console.log('hello');
-//     }
-// }
+class Son2 extends Person1 {
+    constructor() {
+        super();
+    }
+}
 
-// console.log(Person5.uname);
-
-
-// interface IPerson6 {
-//     uname: string;
-//     say(a: number): void;
-// }
-
-// // 抽象类
-// abstract class Person6 {
-//     uname: string;
-
-//     abstract say(a: number): void;
-//     say1() {
-//         console.log('hello');
-//     }
-// }
-
-// class Son6 extends Person6 {
-//     say(a: number): void {
-//         console.log('hello say');
-//     }
-// }
-
-// class SonI6 implements IPerson6 {
-//     uname: string;
-//     say(a: number): void {
-//         console.log('hello say');
-//     }
-// }
-
-// class Person7 {
-//     uname: string;
-//     age: number;
-//     constructor(uname: string, age: number) {
-//         this.uname = uname;
-//         this.age = age;
-//     }
-// }
-
-// interface IPerson7 extends Person7 {
-//     say(): void;
-//     log(): void;
-// }
-
-// let obj0001: IPerson7 = {
-//     uname: 'jack',
-//     age: 18,
-//     say() {
-//         console.log('hello say');
-//     },
-//     log() {
-//         console.log('hello log');
-//     }
-// }
-
-// let addFun: (a: number, b: number) => number;
-
-// addFun = function (a: number, b: number) {
-//     return a;
-// }
-// addFun = function (a: number) {
-//     return a;
-// }
-
-// // addFun(1) // 编译报错
-// addFun(1, 2)
-
-// interface IAdd {
-//     (a: number, b: number): void;
-// }
-
-// let addFun01 = function (a: number, b: number): number {
-//     return a + b
-// }
-// // 默认值
-// interface IAdd2 {
-//     (a: number, b?: number): void;
-// }
-
-// let addFun02 = function (a: number, b: number = 100): number {
-//     return a + b
-// }
-// addFun02(1, 2)
-// addFun02(1)
-
-// // 剩余参数
-// interface IAdd3 {
-//     (a: number, b?: number, ...rest: number[]): void;
-// }
-
-// let addFun03 = function (a: number, b: number = 100, ...rest: number[]): number {
-//     return a
-// }
-// addFun03(1, 2, 3, 4, 5)
-// addFun03(1, 2)
-// addFun03(1)
-
-// // 函数重载
-// function log1(x: number): number;
-// function log1(x: string): string;
-// function log1(x: any): any {
-//     if (typeof x === 'number') {
-//         return 10 * x;
-//     } else if (typeof x === 'string') {
-//         return 'hello' + x;
-//     }
-// };
-
-// console.log(log1(2));
-// console.log(log1('jack'));
-
-// // 泛型
-// function log<T>(x: T): T {
-//     // console.log(x);
-//     return x;
-// }
-// log<string>('hello');
-// log<number>(123);
+const s002 = new Son2();
 
 
-// function swap<T, U>(arr: [T, U]) {
-//     return [arr[1], arr[0]]
-// }
+// 只读
+class Person2 {
+    readonly uname: string;
+    constructor(uname: string) {
+        this.uname = uname;
+    }
+}
+const p003 = new Person2('jack');
 
-// console.log(swap<number, number>([1, 2]));
-// console.log(swap<string, string>(['a', 'b']));
-// console.log(swap<string, number>([' a ', 1]));
+console.log(p003.uname);
 
-// function getLen<T extends { length: number }>(x: T) {
-//     return x.length;
-// }
+class Person3 {
+    constructor(public uname: string, private readonly age: number) { }
+}
 
-// getLen({ length: 10 });
-// getLen([]);
+const p0002 = new Person3('jack', 18);
 
-// interface User1<T, U> {
-//     uname: string;
-//     age: T;
-//     gender: U;
-// }
+// 存取器
 
-// let u1: User1<number, string> = {
-//     uname: 'jack',
-//     age: 20,
-//     gender: 'male'
-// }
+class Person4 {
+    private gender: number = 1;
+    // getGender(){
+    //     return this.gender;
+    // }
+    // setGender(gender:number){
+    //     this.gender = gender;
+    // }
+    // 优化后写法
+    get Gender() {
+        return this.gender;
+    }
+    set Gender(gender: number) {
+        this.gender = gender;
+    }
+}
 
-// interface User2<T, U> {
-//     uname: string;
-//     age: number;
-//     gender: U;
-// }
+const p0004 = new Person4();
+console.log(p0004.Gender);
+p0004.Gender = 0;
+console.log(p0004.Gender);
 
-// interface User {
-//     uname: string;
-//     age: number;
-// }
+// 静态属性
+class Person5 {
+    static uname: string = 'jack';
+    static say() {
+        console.log('hello');
+    }
+}
 
-// interface Func1<T> {
-//     <K extends User>(x: T, y: K): T;
-// }
-// let say: Func1<number>;
-// say = function <K extends User>(x: number, y: K): number {
-//     return y.age;
-// }
+console.log(Person5.uname);
 
-// // 类中使用泛型
-// class Users<T = string> {
-//     name: T;
-//     constructor(name: T) {
-//         this.name = name;
-//     }
-// }
-// const p002 = new Users<string>('jack')
-// console.log(p002.name);
+
+interface IPerson6 {
+    uname: string;
+    say(a: number): void;
+}
+
+// 抽象类
+abstract class Person6 {
+    uname: string;
+
+    abstract say(a: number): void;
+    say1() {
+        console.log('hello');
+    }
+}
+
+class Son6 extends Person6 {
+    say(a: number): void {
+        console.log('hello say');
+    }
+}
+
+class SonI6 implements IPerson6 {
+    uname: string;
+    say(a: number): void {
+        console.log('hello say');
+    }
+}
+
+class Person7 {
+    uname: string;
+    age: number;
+    constructor(uname: string, age: number) {
+        this.uname = uname;
+        this.age = age;
+    }
+}
+
+interface IPerson7 extends Person7 {
+    say(): void;
+    log(): void;
+}
+
+let obj0001: IPerson7 = {
+    uname: 'jack',
+    age: 18,
+    say() {
+        console.log('hello say');
+    },
+    log() {
+        console.log('hello log');
+    }
+}
+
+let addFun: (a: number, b: number) => number;
+
+addFun = function (a: number, b: number) {
+    return a;
+}
+addFun = function (a: number) {
+    return a;
+}
+
+// addFun(1) // 编译报错
+addFun(1, 2)
+
+interface IAdd {
+    (a: number, b: number): void;
+}
+
+let addFun01 = function (a: number, b: number): number {
+    return a + b
+}
+// 默认值
+interface IAdd2 {
+    (a: number, b?: number): void;
+}
+
+let addFun02 = function (a: number, b: number = 100): number {
+    return a + b
+}
+addFun02(1, 2)
+addFun02(1)
+
+// 剩余参数
+interface IAdd3 {
+    (a: number, b?: number, ...rest: number[]): void;
+}
+
+let addFun03 = function (a: number, b: number = 100, ...rest: number[]): number {
+    return a
+}
+addFun03(1, 2, 3, 4, 5)
+addFun03(1, 2)
+addFun03(1)
+
+// 函数重载
+function log1(x: number): number;
+function log1(x: string): string;
+function log1(x: any): any {
+    if (typeof x === 'number') {
+        return 10 * x;
+    } else if (typeof x === 'string') {
+        return 'hello' + x;
+    }
+};
+
+console.log(log1(2));
+console.log(log1('jack'));
+
+// 泛型
+function log<T>(x: T): T {
+    // console.log(x);
+    return x;
+}
+log<string>('hello');
+log<number>(123);
+
+
+function swap<T, U>(arr: [T, U]) {
+    return [arr[1], arr[0]]
+}
+
+console.log(swap<number, number>([1, 2]));
+console.log(swap<string, string>(['a', 'b']));
+console.log(swap<string, number>([' a ', 1]));
+
+function getLen<T extends { length: number }>(x: T) {
+    return x.length;
+}
+
+getLen({ length: 10 });
+getLen([]);
+
+interface User1<T, U> {
+    uname: string;
+    age: T;
+    gender: U;
+}
+
+let u1: User1<number, string> = {
+    uname: 'jack',
+    age: 20,
+    gender: 'male'
+}
+
+interface User2<T, U> {
+    uname: string;
+    age: number;
+    gender: U;
+}
+
+interface User {
+    uname: string;
+    age: number;
+}
+
+interface Func1<T> {
+    <K extends User>(x: T, y: K): T;
+}
+let say: Func1<number>;
+say = function <K extends User>(x: number, y: K): number {
+    return y.age;
+}
+
+// 类中使用泛型
+class Users<T = string> {
+    name: T;
+    constructor(name: T) {
+        this.name = name;
+    }
+}
+const p002 = new Users<string>('jack')
+console.log(p002.name);
 
 // 枚举
 enum Direction {
