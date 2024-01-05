f2add0a867de721625eae290d86c25aac2b7e3c2
DR-wyl <wylpeter@qq.com>
2024-01-05 10:54:01 +0800

    update

--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,55 +1,33 @@
 <template>
     <div class="testPage" ref="testRef">
-        <el-date-picker id="kkk" ref="datePickerRef" v-model="value1" @change="changeFun" type="datetimerange"
-            range-separator="To" start-placeholder="Start date" end-placeholder="End date" />
-        <br>
-        <button @click="showDiv = !showDiv">togle</button>
-        <button @click="fun">togle2</button>
-        <!-- <div id="yyy" v-show="showDiv" style="height: 20px;width: 20px;background-color: blanchedalmond;"></div> -->
-        <div id="yyy" v-if="showDiv" style="height: 20px;width: 20px;background-color: blanchedalmond;"></div>
+       <input type="file" @change="fileChange">
+        <img src="http://127.0.0.1:3000/1.jpg?1" alt="">
+        <img :src="imgbse64" alt="">
     </div>
 </template>
   
 <script setup>
-
-import { ref, onMounted, shallowRef } from 'vue'
-
-const showDiv = ref(true);
-const datePickerRef = shallowRef(null);
-const testRef = shallowRef(null);
-function changeFun(value) {
-    console.log(value);
-}
-const value1 = ref('');
-function fun(){
-    document.getElementById('yyy').remove()
-}
-onMounted(async () => {
-    console.log(testRef.value);
-    console.log(testRef.value.querySelectorAll('span'));
-
-    function createMutationObserver(targetNode, callback, config) {
-        const observer = new MutationObserver(callback);
-        config = config || { subtree: true, attributes: true };
-        observer.observe(targetNode, config);
-        return observer;
+import {ref} from 'vue'
+const imgbse64 = ref('')
+function fileChange(e){
+    let file = e.target.files[0];
+    // console.log(file.size);
+    let _slicBlob = new Blob([file]).slice(0, 300 * 1024);
+    let _sliceFile = new File([_slicBlob], file.name, {type: file.type});
+    let fr = new FileReader();
+    fr.readAsDataURL(_sliceFile);
+    fr.onload = function(e) {
+        imgbse64.value = fr.result;
     }
-    // createMutationObserver(document.querySelector('.testPage'), (mutationsList, observer) => {
-    createMutationObserver(document.getElementById('yyy'), (mutationsList, observer) => {
-        console.log(mutationsList,"111");
-        console.log(observer);
-        // observer.disconnect();
-        return;
-    });
-
-});
-
-
+}
 </script>
 <style lang="scss" scoped>
 .testPage {
     box-sizing: border-box;
-    padding: 2px;
+    // padding: 2px;
+    img{
+        width: 600px;
+    }
 }
 </style>
   
