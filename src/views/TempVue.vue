2f9ffff155781d5bc5c1fc8d81eda557e00c32b5
DR-wyl <wylpeter@qq.com>
2024-01-18 14:09:45 +0800

    update

--- src/views/TempVue.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>加载完毕</div>
-</template>
-  
-<script setup>
-const res = await new Promise(r => setTimeout(r, 3000));
-</script>
--- src/views/TestPage.vue
+++ src/views/TestPage.vue
@@ -1,22 +1,21 @@
 <template>
     <div class="testPage" ref="testRef">
-       <input type="file" @change="fileChange">
+        <input type="file" @change="fileChange">
         <img src="http://127.0.0.1:3000/1.jpg?1" alt="">
         <img :src="imgbse64" alt="">
     </div>
 </template>
   
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 const imgbse64 = ref('')
-function fileChange(e){
+function fileChange(e) {
     let file = e.target.files[0];
-    // console.log(file.size);
     let _slicBlob = new Blob([file]).slice(0, 300 * 1024);
-    let _sliceFile = new File([_slicBlob], file.name, {type: file.type});
+    let _sliceFile = new File([_slicBlob], file.name, { type: file.type });
     let fr = new FileReader();
     fr.readAsDataURL(_sliceFile);
-    fr.onload = function(e) {
+    fr.onload = function (e) {
         imgbse64.value = fr.result;
     }
 }
@@ -24,8 +23,7 @@ function fileChange(e){
 <style lang="scss" scoped>
 .testPage {
     box-sizing: content-box;
-    // padding: 2px;
-    img{
+    img {
         width: 600px;
     }
 }
